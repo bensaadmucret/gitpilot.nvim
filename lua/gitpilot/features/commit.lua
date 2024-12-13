3b7cc0f8d7fecd549a4855d7ae9ae9a269a4ed48
bensaadmucret <bensaadmucret@gmail.com>
2024-12-13 23:02:23 +0100

    fix: ensure consistent error and success messages in amend_commit_builtin

--- lua/gitpilot/features/commit.lua
+++ lua/gitpilot/features/commit.lua
@@ -262,7 +262,9 @@ local function amend_commit_builtin(callback)
         local amend_success, output = utils.execute_command(cmd)
 
         if not amend_success then
-            return handle_amend_error(output, callback)
+            ui.show_error(i18n.t('commit.error.amend_failed') .. "\n" .. output)
+            if callback then callback(false) end
+            return false
         end
 
         ui.show_success(i18n.t('commit.success.amended'))
